cfabd3add9db4bda3e6761df9dbd369fc35d789a
misohan <michal.hanusniak@aiesec.cz>
2019-12-08 11:01:03 +0100

    Edit function 'introuduce' in file

--- gittie.py
+++ gittie.py
@@ -1,13 +1,11 @@
 def introduce():
     print("Hello, I'm Gittie!")
+    print("This is pure conflict")
 
-<<<<<<< HEAD
 def add():
     imie = input("Podaj swoje imie koleżko/koleżanko: ")
     print(imie + " to bardzo ładne imię!")
 
 add()
-=======
 def joke():
     print("This function is joke")
->>>>>>> 897cbed63996c9ac86d086d3e2fb8e3ba17a44dd
